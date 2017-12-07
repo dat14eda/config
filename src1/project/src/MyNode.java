d40ae90a144a15f4dcb087dd7ba7e7ae0830cb22
erik danielsson <erik.danielsson@student.lu.se>
2017-12-07 14:23:54 +0100

    heybabysexybaby # This is a combination of 2 commits.
    hey baby
    
    sexybaby

--- src1/project/src/MyNode.java
+++ src1/project/src/MyNode.java
@@ -18,7 +18,9 @@ public class MyNode {
    * @param value The value of this node.
    */
   public MyNode(int value) {
-    this.value = value+1;
+    this.value = value+5;
+    this.value = 420;
+    this.babyboy = sexy;
   }
 
   /**
